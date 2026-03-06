628b879ca8c3eef21751f10a439818b8bab8cf4c
Kiran G V <kirangv121@gmail.com>
2026-03-06 23:07:14 +0530

    Activity2

---
+++ Activity_Assignments_code/Activity2_Assignment2/Activity2.js
@@ -0,0 +1,17 @@
+// Creating a JavaScript object using object literal
+
+let student = {
+    name: "Kirana G V",
+    college: "Cambridge Institute of Technology North Campus",
+    branch: "Computer Science",
+    year: "Final Year",
+    interest: "Software Development"
+};
+
+// Accessing object properties
+
+console.log("Student Name:", student.name);
+console.log("College:", student.college);
+console.log("Branch:", student.branch);
+console.log("Year:", student.year);
+console.log("Interest:", student.interest);
