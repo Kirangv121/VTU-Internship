14598c5ae3c5b5b592fa9807b8ae96733e3f8635
Kiran G V <kirangv121@gmail.com>
2026-03-11 23:42:56 +0530

    props

---
+++ Reactjs/Mar2_to_Mar7/src/Mar2/Exam.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+function Exam(){
+
+	return(
+		<>
+
+        <h1>Exam Details</h1>
+	 
+		
+		
+		</>
+	)
+
+}
+
+
+export default Exam
---
+++ Reactjs/Mar2_to_Mar7/src/Mar2/Student.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Exam from './Exam';
+
+function Student(){
+
+	return(
+		<>
+
+		<Exam/>
+		
+		
+		</>
+	)
+
+}
+
+
+export default Student
