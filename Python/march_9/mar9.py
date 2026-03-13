4e6fa1634b935cfd11a3b90198eb96f6f5c4010f
Kiran G V <kirangv121@gmail.com>
2026-03-13 13:47:05 +0530

    Variables and Data Types code updated

---
+++ Python/march_9/mar9.py
@@ -0,0 +1,44 @@
+# Variable Declaration
+name = "Python"
+age = 21
+price = 99.99
+
+print(name)
+print(age)
+print(price)
+
+
+# Dynamic Typing Example
+x = 10
+print(x)
+
+x = "Ten"
+print(x)
+
+
+# Type Checking
+a = 25
+b = 10.5
+c = "Hello"
+d = True
+e = None
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+
+
+# Data Types Examples
+num = 100          # int
+temperature = 36.7 # float
+language = "Python" # str
+is_active = True   # bool
+data = None        # NoneType
+
+print(num)
+print(temperature)
+print(language)
+print(is_active)
+print(data)
