d256a12b894014957494ff89178874e18d73a8de
Kiran G V <kirangv121@gmail.com>
2026-03-17 07:50:37 +0530

    Nested Condition Demonstration Activity week 5

---
+++ Activity_Assignments_code/Week5/Activity5.py
@@ -0,0 +1,14 @@
+# Nested Condition Demonstration
+
+number = int(input("Enter a number: "))
+
+if number > 0:
+    print("The number is positive.")
+
+    if number % 2 == 0:
+        print("The number is even.")
+    else:
+        print("The number is odd.")
+
+else:
+    print("The number is not positive.")
