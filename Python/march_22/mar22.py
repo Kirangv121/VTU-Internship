0e34cb7ceb566d9c530cb846eea31f8363d0fad9
Kiran G V <kirangv121@gmail.com>
2026-03-22 20:04:30 +0530

    Dictionary Methods code updated

---
+++ Python/march_22/mar22.py
@@ -0,0 +1,43 @@
+# Creating Dictionary
+student = {
+    "name": "Rahul",
+    "age": 20,
+    "course": "Python"
+}
+
+print("Original Dictionary:", student)
+
+
+# keys()
+print("\nKeys:")
+print(student.keys())
+
+
+# values()
+print("\nValues:")
+print(student.values())
+
+
+# items()
+print("\nItems:")
+print(student.items())
+
+
+# get()
+print("\nUsing get():")
+print(student.get("name"))
+print(student.get("marks"))  # key not present
+
+
+# update()
+print("\nUsing update():")
+student.update({"age": 21})
+student.update({"city": "Bangalore"})
+print(student)
+
+
+# pop()
+print("\nUsing pop():")
+removed = student.pop("age")
+print("Removed value:", removed)
+print("Updated Dictionary:", student)
