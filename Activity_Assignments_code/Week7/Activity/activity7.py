4d485c554e25f289c58e548dae548aad2705e289
Kiran G V <kirangv121@gmail.com>
2026-03-25 10:27:16 +0530

    activity 7 tuple iteration  code updated

---
+++ Activity_Assignments_code/Week7/Activity/activity7.py
@@ -0,0 +1,10 @@
+# Tuple Iteration Practice
+
+# Creating a tuple of city names
+cities = ("Bengaluru", "Mumbai", "Delhi", "Chennai", "Hyderabad")
+
+print("List of Cities:")
+
+# Iterating through the tuple
+for city in cities:
+    print(city)
