e772539b4375451d245e21dee6f9f9e534212150
Kiran G V <kirangv121@gmail.com>
2026-03-27 16:13:18 +0530

    Activity 8

---
+++ Activity_Assignments_code/week8/activity8.py
@@ -0,0 +1,27 @@
+# Multithreading Demonstration Program
+
+import threading
+
+# Function to print numbers
+def print_numbers():
+    for i in range(1, 6):
+        print("Number:", i)
+
+# Function to print letters
+def print_letters():
+    for ch in ['A', 'B', 'C', 'D', 'E']:
+        print("Letter:", ch)
+
+# Creating threads
+thread1 = threading.Thread(target=print_numbers)
+thread2 = threading.Thread(target=print_letters)
+
+# Starting threads
+thread1.start()
+thread2.start()
+
+# Waiting for threads to complete
+thread1.join()
+thread2.join()
+
+print("Both threads have finished execution.")
