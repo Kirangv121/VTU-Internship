5e302434f55e0c1da6fb4abc730ef2ed63108c68
Kiran G V <kirangv121@gmail.com>
2026-03-27 16:15:01 +0530

    Assignment 8 - # Encapsulation and Data Protection Program

---
+++ Activity_Assignments_code/week8/assignment8.py
@@ -0,0 +1,38 @@
+# Encapsulation and Data Protection Program
+
+class BankAccount:
+    
+    def __init__(self, balance):
+        # Private variable
+        self.__balance = balance
+
+    # Method to deposit money
+    def deposit(self, amount):
+        self.__balance += amount
+        print("Deposited:", amount)
+
+    # Method to withdraw money
+    def withdraw(self, amount):
+        if amount <= self.__balance:
+            self.__balance -= amount
+            print("Withdrawn:", amount)
+        else:
+            print("Insufficient balance")
+
+    # Getter method to access balance
+    def get_balance(self):
+        return self.__balance
+
+
+# Creating object
+account = BankAccount(1000)
+
+# Performing operations
+account.deposit(500)
+account.withdraw(300)
+
+# Accessing balance using getter
+print("Current Balance:", account.get_balance())
+
+# Trying to access private variable directly (not allowed)
+# print(account.__balance)  # This will cause an error
